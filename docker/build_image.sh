deb3acf62d5e22d8b652a5d3dc6e33d33a4d1179
Krzysztof Marciniak <krzysztof.s.marciniak@student.put.poznan.pl>
2016-01-19 20:59:23 +0100

    Another sync

---
+++ docker/build_image.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t misra-token --rm=true .
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule MisraToken.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: [main_module: MisraToken],
      deps: deps]
   end
 
