7973116d68822f3a97bc6c1eec1535d76f7752b6
Krzysztof Marciniak <krzysztof.s.marciniak@student.put.poznan.pl>
2016-01-19 23:51:52 +0100

    Another sync

--- lib/misra.ex
+++ lib/misra.ex
@@ -9,25 +9,52 @@ defmodule MisraToken do
   defp regenerate(next, x), do: propagate next, [{:ping, abs(x)}, {:pong, -abs(x)}]
   defp incarnate(next, x), do: propagate next, [{:ping, abs(x)+1}, {:pong, -abs(x)-1}]
   
-  def cs(i) do
+  def cs(i, coordinator) do
+    send coordinator, {:cs_enter, i}
     IO.puts "entering CS on " <> to_string i
     :timer.sleep 1000
+    send coordinator, {:cs_exit, i}
     IO.puts "leaving CS on " <> to_string i
   end
 
   def meeting(m, value), do: m*value < 0
 
-  def start(i, next) do
-    if i == 0, do: propagate self, [{:ping, 1}, {:pong, -1}]
-    loop i, next, 0
+  def startNodes(ips, ids, next) when ips != [] do
+    [ip_head|ip_tail] = ips
+    [id_head|id_tail] = ids
+    [nx_head|nx_tail] = next
+
+    name = String.to_atom("misra@" <> ip_head)
+    Node.ping name
+    Node.spawn_link name, MisraToken, :start, id_head, nx_head, self
+
+    startNodes ip_tail, id_tail, nx_tail
+  end
+  def startNodes(ips, ids, next) when ips == [], do: :ok
+
+  def coordStart(ips, ids, next) do
+    # ...
+    startNodes ips, ids, next
+
+    receive do
+      {:cs_enter, id} ->
+        IO.puts to_string(id) <> " entering cs"
+      {:cs_exit, id} ->
+        IO.puts to_string(id) <> " leaving cs"
+    end
   end
 
-  def loop(i, next, m) do
+  def start(i, next, coordinator) do
+    if i == 0, do: propagate self, [{:ping, 1}, {:pong, -1}]
+    loop i, next, 0, coordinator
+  end
+
+  def loop(i, next, m, coordinator) do
     receive do
       {:ping, value} ->
         if m == value, do: regenerate next, value
 
-        cs(i)
+        cs(i, coordinator)
         if meeting(m, value), do: incarnate next, value
 
         send next, {:ping, value+1}
@@ -44,6 +71,18 @@ defmodule MisraToken do
     end
   end
 
+  def nodePid(ip_addr) do
+    name = String.atom("misra@" <> ip_addr)
+    Node.spawn_link name, fn -> :ok end
+  end
+
+  def init(next_ip, id) do
+    pid = nodePid next_ip
+
+    :timer.sleep 5000
+    start id, pid
+  end
+
   def main(args) do
     switches = [id: :integer, count: :integer, ip: :string, next: :string]
     {options, _, _} = OptionParser.parse args, switches: switches
@@ -58,11 +97,10 @@ defmodule MisraToken do
       next_ip = Dict.fetch! options, :next
 
       Node.start String.to_atom("misra@" <> my_ip)
-      next = String.atom("misra@" <> next_ip)
-      Node.ping node
-
-      pid = Node.spawn_link next, fn -> :ok end
       
+      pid = nodePid next_ip
+      
+      :timer.sleep 5000
       MisraToken.start id, pid
     end
   end
--- startup.sh
+++ startup.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 NODE_COUNT=$1
-COMMAND="export IP_ADDR=\`ip a | tail -4 | head -1 | tr -s \" \" | cut -d\" \" -f3 | cut -d/ -f1\` && iex --name \\\"cohort@\$IP_ADDR\\\" --cookie test"
+COMMAND=""
 
-for NODE_ID in {1..$NODE_COUNT}
+CONTS=$(sudo docker ps -a | grep misra | cut -d' ' -f1)
+if [ ! -z "$CONTS" ]
+then
+    echo "$CONTS" | xargs sudo docker rm -f
+fi
+
+for NODE_ID in $(seq 1 $NODE_COUNT)
 do
-    docker run --name "misra-node-$NODE_ID" -t misra-token $COMMAND
+    sudo docker run -d --name "misra-node-$NODE_ID" -t misra-token /bin/bash -c '(export IP_ADDR=`ip a | tail -4 | head -1 | tr -s " " | cut -d" " -f3 | cut -d/ -f1` && iex --name "cohort@$IP_ADDR" --cookie test)'
 done
+IP_ADDRS=($(sudo docker ps | grep misra | tr -s ' ' | sort -k12 | cut -d' ' -f1 | xargs sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' | sed ':a;N;$!ba;s/\r\n/ /g'))
+CONT_IDS=($(sudo docker ps | grep misra | tr -s ' ' | sort -k12 | cut -d' ' -f1))
+
+function joinString { local IFS="$1"; shift; echo "[\"$*\"]"; }
+function joinInt { local IFS="$1"; shift; echo "[$*]"; }
+
+IPS=$(joinString "\", \"" "${IP_ADDRS[@]}")
+IDS=$(joinInt ", " $(seq 0 $(($NODE_COUNT-1))))
+NEXT_IPS=$(seq 1 $NODE_COUNT)
+
+for i in $(seq 1 $NODE_COUNT)
+do
+    NEXT_IPS[$i] = ${IP_ADDRS[$(($i % $NODE_COUNT))]}
+done
+NEXT_IPS_STR=$(joinString "\", \"" "${NEXT_IPS[@]}")
+
+iex -S mix run -e "MisraToken.coordStart $IDS, $IPS, $NEXT_IPS"
+#for i in $(seq 1 $NODE_COUNT)
+#do
+    #echo "${CONT_IDS[(($i-1))]} -> ${IP_ADDRS[$(($i % $NODE_COUNT))]}"
+#done
+
+#iex -S mix -e ''
